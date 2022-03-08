ad1f419663187055f3186c42439666a19594351c
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-03-08 06:28:18 -0500

    Nav Bar Added

---
+++ opensea-clone/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Header from "../components/Header";
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Header/>
+}
+
+export default MyApp
