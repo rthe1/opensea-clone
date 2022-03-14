f89dd507cd49d11f32f7ce1f57a5b95fff19b13c
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-03-14 00:13:19 -0400

    collection nav updated

--- opensea-clone/pages/_app.tsx
+++ opensea-clone/pages/_app.tsx
@@ -2,7 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from "../components/Header";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Header/>
+  return (
+    <div>
+      <Header></Header>
+    <Component {...pageProps} />
+  </div>
+  )
 }
 
 export default MyApp
